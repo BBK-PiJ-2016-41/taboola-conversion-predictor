21bdbaf039af1cff1d767ff8328d8643bdf5d33e
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-07-25 21:30:14 +0100

    Connector ABC and TaboolaConnector

---
+++ venv/conversion-predictor/Connector.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Connector(ABC):
+
+
+    def __init__(self):
+        TODO
+
+    @abstractmethod
+    def set_start_date(self, date):
+        TODO
+
+    @abstractmethod
+    def set_end_date(self, date):
+        TODO
+
+    @abstractmethod
+    def get_campaign_ids(self, file):
+        TODO
+
+    @abstractmethod
+    def get_data(self):
+        TODO
---
+++ venv/conversion-predictor/TaboolaConnector.py
@@ -0,0 +1,17 @@
+class TaboolaConnector(Connector):
+
+
+    def __init__(self):
+        TODO
+
+    def set_start_date(self, date):
+        TODO
+
+    def set_end_date(self, date):
+        TODO
+
+    def get_campaign_ids(self, file):
+        TODO
+
+    def get_data(self):
+        TODO
