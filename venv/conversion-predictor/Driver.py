e85e5fc24356088794049ccc78c06c89acc1cd1f
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-07-25 21:07:33 +0100

    Initialised project and created driver class

---
+++ venv/conversion-predictor/Driver.py
@@ -0,0 +1,9 @@
+class Driver:
+    def __init__(self):
+        TODO
+
+    def run(self):
+        TODO
+        #user should provide date range
+        #prompt user for each category of feature engineering
+        #contains master dataframe
