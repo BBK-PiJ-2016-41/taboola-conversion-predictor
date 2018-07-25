fe59b0509459b74a9530b2241772150a6e662d11
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-07-25 21:40:12 +0100

    Model ABC and linear, ridge, lasso empty classes

---
+++ venv/conversion-predictor/Model.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Model(ABC):
+    TODO
+    
+class RidgeRegressionModel(Model):
+    TODO
+    
+class LassoRegression(Model):
+    TODO
+    
+class LinearRegression(Model):
+    TODO
