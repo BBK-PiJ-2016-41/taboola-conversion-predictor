17a458610d0b8dca2a9fb1af032a5b9afdc78170
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-07-25 21:40:49 +0100

    Moved Taboola Connector into Connector module

--- venv/conversion-predictor/Connector.py
+++ venv/conversion-predictor/Connector.py
@@ -25,3 +25,24 @@ class Connector(ABC):
     @abstractmethod
     def get_credentials(self, authenticator):
         TODO
+
+
+class TaboolaConnector(Connector):
+
+    def __init__(self):
+        TODO
+
+    def set_start_date(self, date):
+        TODO
+
+    def set_end_date(self, date):
+        TODO
+
+    def get_campaign_ids(self, file):
+        TODO
+
+    def get_data(self):
+        TODO
+
+    def get_credentials(self, authenticator):
+        TODO
