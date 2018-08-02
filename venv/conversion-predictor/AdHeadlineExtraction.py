5d9d63bf4d9344ff2807f2081781d433a6b67cf3
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-02 21:43:18 +0100

    First test for Ad Headline (number of params) created and test failed

--- venv/conversion-predictor/AdHeadlineExtraction.py
+++ venv/conversion-predictor/AdHeadlineExtraction.py
@@ -1,8 +1,17 @@
 class AdExtractor:
 
+    def __init__(self, dataframe):
+        """
+        Constructor for AdExtractor.
+        :param dataframe: A dataframe of two columns containing the ad_id and headline_text
+        """
+        self.df = dataframe
 
     def num_dynamic_params(self):
         TODO
 
     def num_punc_marks(self):
+        TODO
+
+    def run_all(self):
         TODO
---
+++ venv/conversion-predictor/Tests.py
@@ -0,0 +1,14 @@
+from AdHeadlineExtraction import AdExtractor
+import unittest
+from unittest import TestCase
+import pandas as pd
+
+class HeadlineTester(TestCase):
+    def test_count_params(self):
+        headline_to_test = pd.DataFrame(columns=["ad_id, headline_text"], data=["1", "Over 55s in ${city:capitalized}$ Can Now Use This Free Equity Release Calculator"])
+        ad_extractor = AdExtractor(headline_to_test)
+        print(ad_extractor.num_dynamic_params())
+        self.assertEquals(2, ad_extractor.num_dynamic_params())
+
+if __name__ == '__main__':
+    unittest.main()
