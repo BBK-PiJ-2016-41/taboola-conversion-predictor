67e5fdaf3fd0c0e7bb8136b065f8ba93a2ebeb9d
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-12 12:41:04 +0100

    test count puncs and zero puncs passed

--- Tests/AdHeadlineExtractionTests.py
+++ Tests/AdHeadlineExtractionTests.py
@@ -18,14 +18,14 @@ class HeadlineTester(TestCase):
         self.assertEquals(0, ad_extractor.num_dynamic_params().iloc[0]['num_params'])
 
     def test_count_puncs(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id", "headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator!"])
-        headline_to_test.set_index("ad_id")
+        headline_to_test = pd.DataFrame({'ad_id': ['1'], 'headline_text': ["Over 55s in London Can Now Use This Free Equity Release Calculator!"]})
+        headline_to_test.set_index('ad_id')
         ad_extractor = AdExtractor(headline_to_test)
         self.assertEquals(1, ad_extractor.num_punc_marks().iloc[0]['num_puncs'])
 
     def test_count_zero_puncs(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id", "headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
-        headline_to_test.set_index("ad_id")
+        headline_to_test = pd.DataFrame({'ad_id': ['1'], 'headline_text': ["Over 55s in London Can Now Use This Free Equity Release Calculator"]})
+        headline_to_test.set_index('ad_id')
         ad_extractor = AdExtractor(headline_to_test)
         self.assertEquals(0, ad_extractor.num_punc_marks().iloc[0]['num_puncs'])
 
--- conversion_predictor/AdHeadlineExtraction.py
+++ conversion_predictor/AdHeadlineExtraction.py
@@ -2,14 +2,15 @@ import pandas as pd
 import numpy as np
 import re
 
-class AdExtractor:
 
-    def __init__(self, dataframe):
+class AdExtractor:
+    def __init__(self, data_frame):
         """
         Constructor for AdExtractor.
-        :param dataframe: A dataframe of two columns containing the ad_id and headline_text
+        :param data_frame: A data_frame of two columns containing the ad_id and headline_text
         """
-        self.df = dataframe
+        self.df = data_frame
+        self.punctuation = r'[,?!.:;&]'
 
     def num_dynamic_params(self):
         return_df = self.df
@@ -17,7 +18,9 @@ class AdExtractor:
         return return_df
 
     def num_punc_marks(self):
-        TODO
+        return_df = self.df
+        return_df['num_puncs'] = return_df['headline_text'].apply(lambda row: len(re.findall(self.punctuation, row)))
+        return return_df
 
     def run_all(self):
         TODO
