db60e2763d5835e9660dc5e5be806a2529ae24ae
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-12 12:05:50 +0100

    test count params passed

--- Tests/AdHeadlineExtractionTests.py
+++ Tests/AdHeadlineExtractionTests.py
@@ -2,30 +2,36 @@ from conversion_predictor.AdHeadlineExtraction import AdExtractor
 import unittest
 from unittest import TestCase
 import pandas as pd
+import numpy as np
 
 class HeadlineTester(TestCase):
     def test_count_params(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id, headline_text"], data=["1", "Over 55s in ${city:capitalized}$ Can Now Use This Free Equity Release Calculator"])
+        headline_to_test = pd.DataFrame({'ad_id': ['1'], 'headline_text': ["Over 55s in ${city:capitalized}$ Can Now Use This Free Equity Release Calculator"]})
+        headline_to_test = headline_to_test.set_index(['ad_id'])
         ad_extractor = AdExtractor(headline_to_test)
-        self.assertEquals(1, ad_extractor.num_dynamic_params().iloc[0]['num_params'])
+        self.assertEqual(1, ad_extractor.num_dynamic_params().iloc[0]['num_params'])
 
     def test_count_zero_params(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id, headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
+        headline_to_test = pd.DataFrame(columns=["ad_id", "headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
+        headline_to_test.set_index("ad_id")
         ad_extractor = AdExtractor(headline_to_test)
         self.assertEquals(0, ad_extractor.num_dynamic_params().iloc[0]['num_params'])
 
     def test_count_puncs(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id, headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator!"])
+        headline_to_test = pd.DataFrame(columns=["ad_id", "headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator!"])
+        headline_to_test.set_index("ad_id")
         ad_extractor = AdExtractor(headline_to_test)
         self.assertEquals(1, ad_extractor.num_punc_marks().iloc[0]['num_puncs'])
 
     def test_count_zero_puncs(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id, headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
+        headline_to_test = pd.DataFrame(columns=["ad_id", "headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
+        headline_to_test.set_index("ad_id")
         ad_extractor = AdExtractor(headline_to_test)
         self.assertEquals(0, ad_extractor.num_punc_marks().iloc[0]['num_puncs'])
 
     def test_count_all(self):
-        headline_to_test = pd.DataFrame(columns=["ad_id, headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
+        headline_to_test = pd.DataFrame(columns=["ad_id", "headline_text"], data=["1", "Over 55s in London Can Now Use This Free Equity Release Calculator"])
+        headline_to_test.set_index("ad_id")
         ad_extractor = AdExtractor(headline_to_test)
         result = ad_extractor.run_all()
         self.assertEquals(0, result.iloc[0]['num_params'])
--- conversion_predictor/AdHeadlineExtraction.py
+++ conversion_predictor/AdHeadlineExtraction.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import re
+
 class AdExtractor:
 
     def __init__(self, dataframe):
@@ -8,7 +12,9 @@ class AdExtractor:
         self.df = dataframe
 
     def num_dynamic_params(self):
-        TODO
+        return_df = self.df
+        return_df['num_params'] = return_df['headline_text'].apply(lambda row: len(re.findall(r'\${', row)))
+        return return_df
 
     def num_punc_marks(self):
         TODO
