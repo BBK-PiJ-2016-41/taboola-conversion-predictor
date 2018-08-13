f3ca77023c04e92692edcd8cb8a28808f1b94cfa
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-13 12:50:28 +0100

    Tests for URL Transformer functions - failed

--- Tests/ArticleTextExtractionTests.py
+++ Tests/ArticleTextExtractionTests.py
@@ -6,16 +6,28 @@ import pandas as pd
 class UrlTester(TestCase):
     #
     def test_contains_ad_id(self):
-        headline_to_test = pd.DataFrame({'ad': ['1'], 'url': ["http://insurance.expertsinmoney.com/private-medical-insurance-sweeping-uk"]})
+        url_to_test = pd.DataFrame({'ad': ['1'], 'url': ["http://insurance.expertsinmoney.com/private-medical-insurance-sweeping-uk"]})
         with self.assertRaises(KeyError):
-            UrlTransformer(headline_to_test)
+            UrlTransformer(url_to_test)
 
     def test_contains_url(self):
-        headline_to_test = pd.DataFrame({'ad_id': ['1'], 'urloops': ["http://insurance.expertsinmoney.com/private-medical-insurance-sweeping-uk"]})
+        url_to_test = pd.DataFrame({'ad_id': ['1'], 'urloops': ["http://insurance.expertsinmoney.com/private-medical-insurance-sweeping-uk"]})
         with self.assertRaises(KeyError):
-            UrlTransformer(headline_to_test)
+            UrlTransformer(url_to_test)
 
     def test_headline_not_text(self):
-        headline_to_test = pd.DataFrame({'ad_id': ['1'], 'url': [2]})
+        url_to_test = pd.DataFrame({'ad_id': ['1'], 'url': [2]})
         with self.assertRaises(ValueError):
-            UrlTransformer(headline_to_test)
+            UrlTransformer(url_to_test)
+
+    def test_html_produced(self):
+        url_to_test = pd.DataFrame({'ad_id': ['1'], 'url': ["http://insurance.expertsinmoney.com/private-medical-insurance-sweeping-uk"]})
+        transformer = UrlTransformer(url_to_test)
+        result = transformer.extract_html()
+        self.assertTrue('<' in result.iloc[0]['html'])
+
+    def test_url_extracted(self):
+        url_to_test = pd.DataFrame({'ad_id': ['1'], 'url': ["http://insurance.expertsinmoney.com/private-medical-insurance-sweeping-uk"]})
+        transformer = UrlTransformer(url_to_test)
+        result = transformer.extract_domains()
+        self.assertEquals(1.0, result.iloc[0]['expertsinmoney'])
