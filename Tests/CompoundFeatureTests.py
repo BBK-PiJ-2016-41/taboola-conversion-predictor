a3ecb426b32fdcdc7468aa6d097533d313ebcd46
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-17 18:28:34 +0100

    test for ad id passed

--- Tests/CompoundFeatureTests.py
+++ Tests/CompoundFeatureTests.py
@@ -7,15 +7,15 @@ class TaboolaConnectorTests(TestCase):
 
     def test_compound_feature_generator(self):
         test_data = pd.DataFrame({'ad_id': ['1'], 'cpc': [0.25], 'ctr': [0.10]})
-        self.feature_calculator = CompoundFeatureCalculator(test_data)
+        feature_calculator = CompoundFeatureCalculator(test_data)
         self.assertEqual(0.025, )
 
     def test_for_ad_id(self):
         with self.assertRaises(KeyError):
-            pd.DataFrame({'ad': ['1'], 'cpc': [0.25], 'ctr': [0.10]})
+            CompoundFeatureCalculator(pd.DataFrame({'ad': ['1'], 'cpc': [0.25], 'ctr': [0.10]}))
 
     def test_for_bad_columns(self):
         test_data = pd.DataFrame({'ad_id': ['1'], 'cpc': [0.25], 'ctr': [0.10]})
-        self.feature_calculator = CompoundFeatureCalculator(test_data)
+        feature_calculator = CompoundFeatureCalculator(test_data)
         with self.assertRaises(KeyError):
             self.feature_calculator.calc_compound('cpx', 'ctx')
