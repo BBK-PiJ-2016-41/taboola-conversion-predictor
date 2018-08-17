dedd11bb1944e6b5eac35511dffe953426a9d43c
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-17 18:39:26 +0100

    docstrings for compound feature calculator

--- conversion_predictor/CompoundFeatureCalculator.py
+++ conversion_predictor/CompoundFeatureCalculator.py
@@ -1,12 +1,22 @@
 class CompoundFeatureCalculator:
 
     def __init__(self, data_frame):
+        """
+        Constructor for Compound Feature Calculator
+        :param data_frame: a dataframe containing the ad id and the columns needing combining.
+        """
         try:
             self.df = data_frame.set_index('ad_id')
         except KeyError:
             raise
 
     def calc_compound(self, col1, col2):
+        """
+        Creates a compound feature based on the two specified columns by multiplying their values together.
+        :param col1: The first column name
+        :param col2: The second column name
+        :return: a dataframe containing the new combined column
+        """
         columns = self.df.columns.values
         if col1 not in columns or col2 not in columns:
             raise KeyError('Please specify columns contained in your dataframe: ' + ' '.join(columns))
