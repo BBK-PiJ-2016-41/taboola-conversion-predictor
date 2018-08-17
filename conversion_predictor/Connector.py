f4a3cd2eddee64e0d6f5e251e8e6a3824cd03d01
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-17 17:42:29 +0100

    all date tests passed

--- conversion_predictor/Connector.py
+++ conversion_predictor/Connector.py
@@ -36,12 +36,18 @@ class TaboolaConnector(Connector):
         self.address = "https://backstage.taboola.com/backstage/api/1.0/mvfglobal-network/reports/top-campaign-content/dimensions/item_breakdown"
         self.auth = "Bearer CdAAAAAAAAAAEYHpAAAAAAAAGAEgACk6tFnBZAEAADooZTI5ODUxMWY3ODJjODZmMWQxOGUyODQ0YTMxMjA0ZThiZDAyMmQ1MkAC::56b9cb::b263aa"
         self.campaigns = []
+        self.start_date = ""
+        self.end_date = ""
 
     def set_start_date(self, date):
-        TODO
+        if len(date) != 10 or '-' not in date:
+            raise ValueError('Please make sure your date is in the format YYYY-MM-DD')
+        self.start_date = date
 
     def set_end_date(self, date):
-        TODO
+        if len(date) != 10 or '-' not in date:
+            raise ValueError('Please make sure your date is in the format YYYY-MM-DD')
+        self.end_date = date
 
     def get_campaign_ids(self, file):
         TODO
