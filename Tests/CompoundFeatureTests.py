5fb9b5c54e50ad99ef7a54b07d6a69fbd094688d
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-17 18:27:15 +0100

    Compound feature calculator tests failed

---
+++ Tests/CompoundFeatureTests.py
@@ -0,0 +1,21 @@
+from conversion_predictor.CompoundFeatureCalculator import CompoundFeatureCalculator
+import pandas as pd
+from unittest import TestCase
+
+
+class TaboolaConnectorTests(TestCase):
+
+    def test_compound_feature_generator(self):
+        test_data = pd.DataFrame({'ad_id': ['1'], 'cpc': [0.25], 'ctr': [0.10]})
+        self.feature_calculator = CompoundFeatureCalculator(test_data)
+        self.assertEqual(0.025, )
+
+    def test_for_ad_id(self):
+        with self.assertRaises(KeyError):
+            pd.DataFrame({'ad': ['1'], 'cpc': [0.25], 'ctr': [0.10]})
+
+    def test_for_bad_columns(self):
+        test_data = pd.DataFrame({'ad_id': ['1'], 'cpc': [0.25], 'ctr': [0.10]})
+        self.feature_calculator = CompoundFeatureCalculator(test_data)
+        with self.assertRaises(KeyError):
+            self.feature_calculator.calc_compound('cpx', 'ctx')
--- conversion_predictor/CompoundFeatureCalculator.py
+++ conversion_predictor/CompoundFeatureCalculator.py
@@ -1,5 +1,14 @@
 class CompoundFeatureCalculator:
 
+    def __init__(self, data_frame):
+        try:
+            if data_frame['headline_text'].dtype != 'object':
+                raise ValueError('Headline text not expected data type - please use text object')
+            self.df = data_frame.set_index('ad_id')
+        except KeyError:
+            raise
+        except ValueError:
+            raise
 
-    def calc_compound(self):
+    def calc_compound(self, col1, col2):
         TODO
