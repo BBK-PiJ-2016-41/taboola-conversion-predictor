761333009bab42c0ae4c0ed0e9ead252caf6a8f2
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-30 15:07:02 +0100

    Refactored models to reduce repetition

--- conversion_predictor/Model.py
+++ conversion_predictor/Model.py
@@ -1,9 +1,6 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
@@ -13,13 +10,14 @@ from sklearn.linear_model import Lasso
 
 class BasicModel(ABC):
 
-    def __init__(self, data_frame, target_variable_col, split_size=0.3):
+    def __init__(self, data_frame, target_variable_col, model_type, split_size=0.3):
         """
         Constructor for the BasicModel class and subclasses.
         Converts data types in dataframe to floats and fills in any NaN values with the mean of the column.
         Calls the method generate_test_split() to get test subsets.
         :param data_frame: The dataframe containing ad data.
         :param target_variable_col: The column to specify the target variable.
+        :param model_type the model to be used for training
         :param split_size: The ratio of test to training data.
         """
         self.df = data_frame
@@ -29,7 +27,9 @@ class BasicModel(ABC):
         for column in self.columns:
             self.df[column] = self.df[column].astype(float)
         self.df = self.df.fillna(self.df.mean())
+        self.lm = model_type
         self.X, self.y, self.X_train, self.X_test, self.y_train, self.y_test = self.generate_test_split(split_size)
+        self.prediction = ''
 
     def generate_test_split(self, split_size):
         """
@@ -44,44 +44,6 @@ class BasicModel(ABC):
 
     def cross_validation(self, folds):
         TODO
-
-    @abstractmethod
-    def fit_model(self):
-        """
-        Fits the relevant model as specified by the child class.
-        :return:
-        """
-        pass
-
-    @abstractmethod
-    def predict(self):
-        """
-        Predicts model outcome as specified by the child class.
-        :return:
-        """
-        pass
-
-    @abstractmethod
-    def score(self):
-        """
-        Generates the model accuracy score as specified by the child class.
-        :return:
-        """
-        pass
-
-    @abstractmethod
-    def root_mean_squared_error(self):
-        pass
-
-
-
-class LinearRegressionModel(BasicModel):
-
-    def __init__(self, data_frame, target_variable_col, split_size=0.3):
-        super().__init__(data_frame, target_variable_col, split_size)
-        self.lm = LinearRegression()
-        self.fit_model()
-        self.prediction = self.predict()
 
     def fit_model(self):
         self.lm.fit(self.X_train, self.y_train)
@@ -101,28 +63,20 @@ class LinearRegressionModel(BasicModel):
     def print_rmse(self):
         print(self.root_mean_squared_error())
 
+
+class LinearRegressionModel(BasicModel):
+
+    def __init__(self, data_frame, target_variable_col, split_size=0.3):
+        super().__init__(data_frame, target_variable_col, LinearRegression(), split_size)
+        self.fit_model()
+        self.prediction = self.predict()
+
+
 class LassoRegressionModel(BasicModel):
 
     def __init__(self, data_frame, target_variable_col, split_size=0.3, alpha=0.1):
-        super().__init__(data_frame, target_variable_col, split_size)
+        super().__init__(data_frame, target_variable_col, Lasso(self.alpha), split_size)
         self.alpha = alpha
-        self.lm = Lasso(self.alpha)
-
-    # this method returns something when others do not
-    def fit_model(self):
-        coef = self.lm.fit(self.X, self.y).coef_
-        return coef
-
-    def predict(self):
-        self.lm.predict(self.X_test)
-
-    def score(self):
-        score = self.lm.score(self.X_test, self.y_test)
-        print(score)
-        return score
-
-    def root_mean_squared_error(self):
-        pass
 
     def reset_alpha(self, alpha):
         """
@@ -132,28 +86,17 @@ class LassoRegressionModel(BasicModel):
         """
         self.alpha = alpha
         self.lm = Lasso(self.alpha)
+
+    def coef(self):
+        coef = self.lm.fit(self.X, self.y).coef_
+        return coef
 
 
 class RidgeRegressionModel(BasicModel):
 
     def __init__(self, data_frame, target_variable_col, split_size=0.3, alpha=0.1):
-        super().__init__(data_frame, target_variable_col, split_size)
+        super().__init__(data_frame, target_variable_col, Ridge(alpha), split_size)
         self.alpha = alpha
-        self.rm = Ridge(self.alpha)
-
-    def fit_model(self):
-        self.rm.fit(self.X_train, self.y_train)
-
-    def predict(self):
-        self.rm.predict(self.X_test)
-
-    def score(self):
-        score = self.rm.score(self.X_test, self.y_test)
-        print(score)
-        return score
-
-    def root_mean_squared_error(self):
-        pass
 
     def reset_alpha(self, alpha):
         """
@@ -162,4 +105,4 @@ class RidgeRegressionModel(BasicModel):
         :return:
         """
         self.alpha = alpha
-        self.rm = Ridge(self.alpha)
+        self.lm = Ridge(self.alpha)
