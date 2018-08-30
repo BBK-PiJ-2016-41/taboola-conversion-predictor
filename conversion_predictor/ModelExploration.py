fd1adf95e380bf034bc1041551d99201e0cad7d6
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-08-30 17:53:05 +0100

    Updates to model exploration to take two arguments - bug here with package?

--- conversion_predictor/ModelExploration.py
+++ conversion_predictor/ModelExploration.py
@@ -7,7 +7,7 @@ class ModelExploration(cmd.Cmd):
         super(ModelExploration, self).__init__()
         self.model = regression_model
 
-    def do_fit(self):
+    def do_fit(self, arg=None):
         self.model.fit_model()
 
     def help_fit(self):
@@ -15,26 +15,27 @@ class ModelExploration(cmd.Cmd):
         if self.model.__class__.__name__ == 'LassoRegressionModel':
             print('This function will also print the coefficients.')
 
-    def do_predict(self):
+    def do_predict(self, arg=None):
         self.model.predict()
 
     def help_predict(self):
         print('Run this to train the model on the training data set.')
 
-    def do_score(self):
+    def do_score(self, arg=None):
         self.model.score()
 
     def help_score(self):
         print('Outputs the score of the model.')
 
-    def do_alpha(self, alpha):
+    def do_alpha(self, alpha, arg=None):
         if self.model.__class__.__name__ != 'LinearRegressionModel':
             self.model.reset_alpha(alpha)
         else:
             print('Linear regression model does not take an alpha parameter.')
 
     def help_alpha(self):
-        print('Enter a new alpha parameter to tune Lasso and Ridge regression models.')
+        print('Enter a new alpha parameter to tune Lasso and Ridge regression models.'
+              '\nThis method is not available for Linear models.')
 
     def do_EOF(self, line):
         return True
