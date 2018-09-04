3bb9d683e2b6726f355e2273f5e6d875e20515c0
Kathryn Buckley <kathryn.buckley@live.co.uk>
2018-09-04 13:08:51 +0100

    Added test for Factory pattern

--- Tests/TaboolaTokenRefresherTests.py
+++ Tests/TaboolaTokenRefresherTests.py
@@ -1,5 +1,6 @@
 from conversion_predictor.TokenRefresher import TaboolaTokenRefresher
 from unittest import TestCase
+from conversion_predictor.Factory import TokenRefresherFactory
 
 
 class TaboolaTokenRefresherTests(TestCase):
@@ -8,3 +9,9 @@ class TaboolaTokenRefresherTests(TestCase):
         token_refresher = TaboolaTokenRefresher()
         result = token_refresher.refresh_tokens()
         self.assertEqual(200, result[0])
+
+    def test_factory_pattern(self):
+        advertiser = 'mvfglobal-network'
+        factory = TokenRefresherFactory()
+        refresher = factory.get_object('Taboola')
+        self.assertEqual(advertiser, refresher.advertiser)
